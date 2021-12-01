7c26f637c577d2ae71a6d35ecbe57b3261779b09
DILSHA-SAJAN <345567@manappuram.com>
2021-12-01 14:30:46 +0530

    first commiting

---
+++ bin/basics_dart.dart
@@ -0,0 +1,69 @@
+void main() {
+  // print('Hello World');
+  // var a = stdin.readLineSync();
+  // print('Hello $a');
+
+  // print("Enter two numbers");
+  // var input1 = stdin.readLineSync();
+  // var input2 = stdin.readLineSync();
+
+  // var num1 = int.parse(input1!);
+  // var num2 = int.parse(input2!);
+
+  // print('Sum is ${num1+num2}');
+
+  // int a = 10;r
+  // double b =20;
+
+  // num num1 =20.0;
+  // num num2 = 10;
+
+  // String name = 'Dilsha';
+  // String name1 = "This is going great!";
+  // String address =
+  // ''' My name
+  // My Address
+  // My pincode ''';
+  // print(address.length);
+
+  // int num1 = -10;
+  // if(num1>0){print('$num1 is positive'); }
+  // else if(num1==0){print('number is zero'); }
+  // else
+  // { print('$num1 is negative');}
+
+  // for(var i=0;i<10;i++){
+  //   print('Nos from 1 to 10: ${i+1}');
+  // }
+
+  // var array = [12, 'Name', 20.0];
+  // var numberList = [12, 34, 22];
+  // var listToAdd = [00,33,44];
+  // print(numberList.length);
+  // if (numberList.contains(35)) {
+  //   print('List conatins 35');
+  // } else {
+  //   print('List doesnot contain 35');
+  // }
+  // numberList.add(100);
+  // numberList.removeAt(0);
+  // numberList.addAll(listToAdd);
+  // print(numberList.join(' '));
+
+  // List<List<int>> list = [[1,2,3],[4,5,6]];
+  // print('Internal element ${list[1][1]}');
+
+  // List <int> numberList = [1,2,3,4,5,6,7,8,9,1,1];
+  // Set <int> numberSet = {1,2,3,4,5,6,7,8,9,1,1};
+  // print(numberList);
+  // print(numberSet);
+
+  Map<String, String> maps = {
+    "name": "Dilsha",
+    "age": "32",
+    "job": "Developer"
+  };
+
+  print(maps['name']);
+  print(maps['age']);
+}
